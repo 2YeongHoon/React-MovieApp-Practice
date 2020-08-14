396a429e63f74072971b111777964e763d4974d9
Lee <djaakclsrn12@naver.com>
2020-08-15 01:26:38 +0900

    Updates

---
+++ precache-manifest.d5cf2895238e8aa0ed1a415dd4367bb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65f5d4cf5832c9d0e29e408c9e666b0",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "b7b86a622e2878286f8f",
+    "url": "/movie_app_2019/static/css/main.4e90206d.chunk.css"
+  },
+  {
+    "revision": "0f617b210a71c77dda42",
+    "url": "/movie_app_2019/static/js/2.1eecb4d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.1eecb4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b86a622e2878286f8f",
+    "url": "/movie_app_2019/static/js/main.ab4fce2f.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
