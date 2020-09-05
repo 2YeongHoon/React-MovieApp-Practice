532078cde832f9039c0bab12ee3dcc1838e39008
Lee <djaakclsrn12@naver.com>
2020-09-05 15:09:19 +0900

    movie_app 2019 v0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from './components/Navigation';
+import "./App.css"
 
-function App() {
+
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </BrowserRouter>
   );
 }
 
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About(){
+    return <span>About this page: Ibuilt it beacause</span>
+}
+
+export default About;
